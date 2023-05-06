e0a1e5ef794bc39cf76d8c3743d506a01b6da68e
William Carriello <william.provision@gmail.com>
2023-05-06 11:06:11 -0300

    update page

--- pages/index.js
+++ pages/index.js
@@ -45,29 +45,29 @@ export default function Home() {
 
           <div className='w-full mt-6'>
             <Collapse title="Unidade">
-              <p className='mb-2'>A igreja é corpo | 1 Co 12.12</p>
-              <p className='mb-2'>Manifestar Cristo | Jo 17.21</p>
-              <p className='mb-2'>Evitar a Destruição por fatores internos e externos | Mt 12.25</p>
+              <p className="mb-2">A igreja é corpo | 1 Co 12.12</p>
+              <p className="mb-2">Manifestar Cristo | Jo 17.21</p>
+              <p className="mb-2">Evitar a Destruição por fatores internos e externos | Mt 12.25</p>
             </Collapse>
             <Collapse title="Visão">
-              <p className='mb-2'>Conquistar vidas para Cristo</p>
-              <p className='mb-2'>Abrir igrejas</p>
-              <p className='mb-2'>Ajudar outros ministérios</p>
-              <p className='mb-2'>Obras Sociais para soluções definitivas</p>
-              <p className='mb-2'>Construir escolas</p>
-              <p className='mb-2'>Estabelecer o reino na terra</p>
-              <p className='mb-2'>Influenciar em todos os campos da sociedade</p>
+              <p className="mb-2">Conquistar vidas para Cristo</p>
+              <p className="mb-2">Abrir igrejas</p>
+              <p className="mb-2">Ajudar outros ministérios</p>
+              <p className="mb-2">Obras Sociais para soluções definitivas</p>
+              <p className="mb-2">Construir escolas</p>
+              <p className="mb-2">Estabelecer o reino na terra</p>
+              <p className="mb-2">Influenciar em todos os campos da sociedade</p>
               <p>Divisão</p>
             </Collapse>
             <Collapse title="Autoridade">
-              <p className='mb-2'>"Obedecei a vossos pastores e sujeitai-vos a eles; porque velam por vossa alma, como aqueles que hão de dar conta delas; para que o façam com alegria e não gemendo, porque isso não vos seria útil." - Hebreus 13.17</p>
-              <p className='mb-2'>Qualquer organização que não tenha princípio de autoridade, auto se destrói</p>
-              <p className='mb-2'>Deus não negocia princípio de autoridade</p>
-              <p className='mb-2'>A igreja do Apocalipse</p>
-              <p className='mb-2'>Uma vez que o Espírito Santo me Esclareceu este ponto, Ele continuou: "John, quando você estiver perante Mim em julgamento pelo período em que você esteve servindo esta pastor, você não dará conta primeiramente de quantos jovens você levou à salvação em Orlando, Flórida. Você primeiramente será julgado pela sua fidelidade ao pastor sob o qual Eu te coloquei". Pastor John Bevere</p>
+              <p className="mb-2">&quot;Obedecei a vossos pastores e sujeitai-vos a eles; porque velam por vossa alma, como aqueles que hão de dar conta delas; para que o façam com alegria e não gemendo, porque isso não vos seria útil.&quot; - Hebreus 13.17</p>
+              <p className="mb-2">Qualquer organização que não tenha princípio de autoridade, auto se destrói</p>
+              <p className="mb-2">Deus não negocia princípio de autoridade</p>
+              <p className="mb-2">A igreja do Apocalipse</p>
+              <p className="mb-2">Uma vez que o Espírito Santo me Esclareceu este ponto, Ele continuou: &quot;John, quando você estiver perante Mim em julgamento pelo período em que você esteve servindo esta pastor, você não dará conta primeiramente de quantos jovens você levou à salvação em Orlando, Flórida. Você primeiramente será julgado pela sua fidelidade ao pastor sob o qual Eu te coloquei&quot;. Pastor John Bevere</p>
             </Collapse>
             <Collapse title="Palavra de Deus">
-              <p className='mb-2'>Ela é o manual de vida de todo cristão, aquilo que guia nossas escolhas e decisões, ela engloba todos os assuntos acima citados.</p>
+              <p className="mb-2">Ela é o manual de vida de todo cristão, aquilo que guia nossas escolhas e decisões, ela engloba todos os assuntos acima citados.</p>
             </Collapse>
           </div>
 
